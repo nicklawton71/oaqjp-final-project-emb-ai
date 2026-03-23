e2fe3aad9d105a49c17f9af1a8d825da24eed34b
nicklawton71 <nick_lawton@yahoo.co.uk>
2026-03-23 09:46:10 -0400

    Task 6 commit

---
+++ server.py
@@ -0,0 +1,51 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+# Import Flask, render_template, request from the flask pramework package 
+from flask import Flask, render_template, request 
+# Import the emotion_detector function from the package created
+from EmotionDetection.emotion_detection import emotion_detector
+#Initiate the flask app
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def detect_emotion():
+        ''' This code receives the text from the HTML interface and 
+        runs the emotion detector over it using emotion_detector()
+        function. The output returned shows the score for each emotion
+        and the dominant emotion.
+    '''
+    # Retrieve the text to analyze from the request arguments 
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the sentiment_analyzer function and store the response 
+    response = emotion_detector(text_to_analyze)
+
+    # Extract score for each emotion from the response 
+    anger_score = response["anger"]
+    disgust_score = response["disgust"]
+    fear_score = response["fear"]
+    joy_score = response["joy"]
+    sadness_score = response["sadness"]
+    dominant_emotion = response["dominant_emotion"]
+
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {anger_score}, 'disgust': {disgust_score}, "
+        f"'fear': {fear_score}, 'joy': {joy_score} and "
+        f"'sadness': {sadness_score}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    ''' This functions executes the flask app and deploys it on localhost:5000
+    '''
+    app.run(host="0.0.0.0", port=5000)
