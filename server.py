145c1fd41b8563202417dcd3837233a11a2a7d4a
nicklawton71 <nick_lawton@yahoo.co.uk>
2026-03-23 12:42:45 -0400

    Task 8 commit

--- server.py
+++ server.py
@@ -2,8 +2,8 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package 
-from flask import Flask, render_template, request 
+# Import Flask, render_template, request from the flask pramework package
+from flask import Flask, render_template, request
 # Import the emotion_detector function from the package created
 from EmotionDetection.emotion_detection import emotion_detector
 #Initiate the flask app
@@ -11,18 +11,18 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def detect_emotion():
-    ''' This code receives the text from the HTML interface and 
+    ''' This code receives the text from the HTML interface and
         runs the emotion detector over it using emotion_detector()
         function. The output returned shows the score for each emotion
         and the dominant emotion.
     '''
-    # Retrieve the text to analyze from the request arguments 
+    # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
-    # Pass the text to the sentiment_analyzer function and store the response 
+    # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
 
-    # Extract score for each emotion from the response 
+    # Extract score for each emotion from the response
     anger_score = response["anger"]
     disgust_score = response["disgust"]
     fear_score = response["fear"]
@@ -32,14 +32,14 @@ def detect_emotion():
 
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        return (
-            f"For the given statement, the system response is "
-            f"'anger': {anger_score}, 'disgust': {disgust_score}, "
-            f"'fear': {fear_score}, 'joy': {joy_score} and "
-            f"'sadness': {sadness_score}. "
-            f"The dominant emotion is {dominant_emotion}."
-        )
+
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {anger_score}, 'disgust': {disgust_score}, "
+        f"'fear': {fear_score}, 'joy': {joy_score} and "
+        f"'sadness': {sadness_score}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
 
 @app.route("/")
 def render_index_page():
@@ -49,6 +49,5 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
+    #This functions executes the flask app and deploys it on localhost:5000
     app.run(host="0.0.0.0", port=5000)
